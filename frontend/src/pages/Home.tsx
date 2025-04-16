0d8e53c49b31c37c72cdd7d2c9f0a0d1175d4a4c
peynadol <peynadol@gmail.com>
2025-04-16 17:08:09 +0100

    add type to home page - fix typo in db.ts

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import Dashboard from "../components/Dashboard";
+import { Hike } from "@/hike";
 
 export default function Home() {
-  const [allHikes, setAllHikes] = useState([]);
+  const [allHikes, setAllHikes] = useState<Hike[]>([]);
 
   useEffect(() => {
     const fetchHikes = async () => {
@@ -13,5 +14,13 @@ export default function Home() {
     fetchHikes();
   }, []);
 
-  return <Dashboard hikes={allHikes} />;
+  const onDelete = async (id: number) => {
+    const response = await fetch(`http://localhost:3000/api/hikes/${id}`, {
+      method: "DELETE",
+    });
+    console.log(response);
+    setAllHikes(allHikes.filter((hike) => hike.id != id));
+  };
+
+  return <Dashboard hikes={allHikes} onDelete={onDelete} />;
 }
